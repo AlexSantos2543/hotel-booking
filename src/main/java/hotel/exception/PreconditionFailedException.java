8b2ce8985290d5f60177e0cf530db27d4983d763
Alex Santos <alex.santosdosreis@travelport.com>
2020-07-21 20:41:29 +0100

    test commit to master

---
+++ src/main/java/hotel/exception/PreconditionFailedException.java
@@ -0,0 +1,12 @@
+package hotel.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.PRECONDITION_FAILED)
+public class PreconditionFailedException extends RuntimeException {
+
+    public PreconditionFailedException(String message) {
+        super(message);
+    }
+}
